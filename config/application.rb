725eefed9afa9eea694d439d34785d8db65d8679
Matthieu Cartillier <m.carttos@gmail.com>
2017-02-13 18:43:06 +0100

    first rails app

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -10,6 +11,8 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'open-uri'
+require 'json'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'game', to: "games#game"
+  get 'score', to: "games#score"
+  root to: 'games#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
